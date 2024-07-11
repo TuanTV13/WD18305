bea5d5648ed8b4647781f039b3ed6c02623c014e
TuanTV13 <tvtuan01032004@gmail.com>
2024-07-12 00:12:57 +0700

    lab4

--- database/seeders/admin.php
+++ database/seeders/admin.php
@@ -17,30 +17,28 @@ class admin extends Seeder
      */
     public function run(): void
     {
-            $last_name = ['Tuan', 'Nguyen', 'Giang', 'Duong', 'Hung', 'Lan', 'Anh', '', 'Abc', '123', 'Tuna', 'No', 'Yes', 'Pass'];
-            $first_name = ['Tran', 'Nguyen', 'Ly', 'Phan', 'Hoang', 'Pham', 'Ma', 'Thach', 'Hihi', 'Done', 'Pass', 'Lone', 'Pass'];
+        $last_name = ['Tuan', 'Nguyen', 'Giang', 'Duong', 'Hung', 'Lan', 'Anh', '', 'Abc', '123', 'Tuna', 'No', 'Yes', 'Pass'];
+        $first_name = ['Tran', 'Nguyen', 'Ly', 'Phan', 'Hoang', 'Pham', 'Ma', 'Thach', 'Hihi', 'Done', 'Pass', 'Lone', 'Pass'];
 
-            $name = [];
+        $name = [];
 
-            foreach($first_name as $firts){
-                foreach($last_name as $last){
-                    $name[] = $firts.' '. $last;
-                }
+        foreach ($first_name as $firts) {
+            foreach ($last_name as $last) {
+                $name[] = $firts . ' ' . $last;
             }
+        }
 
-            shuffle($name);
+        shuffle($name);
 
-            $randomName = array_slice($name, 0, 100);
+        $randomName = array_slice($name, 0, 100);
 
-            foreach($randomName as $name){
-
-                DB::table('admin')->insert([
-                    'name'=>$name,
-                    'password'=>Str::random(10),
-                    'phone'=>rand(1, 100),
-                    'email'=>Str::random(5).'@gmail.com'
-                ]);
-
-            }
+        foreach ($randomName as $name) {
+            DB::table('admin')->insert([
+                'name' => $name,
+                'password' => Str::random(10),
+                'phone' => rand(1, 100),
+                'email' => Str::random(5) . '@gmail.com'
+            ]);
+        }
     }
 }
